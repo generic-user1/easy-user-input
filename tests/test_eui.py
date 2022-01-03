a2b98181772a6dca40cb0428a5f948d7d3205605
generic-user1 <89677116+generic-user1@users.noreply.github.com>
2022-01-03 16:26:15 -0500

    Fixed issue with direct import of aliased module
    `import easy_user_input.easy_user_input` now works
    
    Added seperate test cases for each form of import

--- tests/test_eui.py
+++ tests/test_eui.py
@@ -1,9 +1,17 @@
 
 
-#test that `easy_user_input.easy_user_input` is 
-#the same module as `easy_user_input.eui`
-def test_easy_user_input_alias():
+#test that `from easy_user_input import easy_user_input` is 
+#the same as `from easy_user_input import eui`
+def test_alias_from():
     from easy_user_input import eui
     from easy_user_input import easy_user_input
     assert eui == easy_user_input
 
+
+#test that `import easy_user_input.easy_user_input` is 
+#the same as `import easy_user_input.eui`
+def test_alias_direct():
+    import easy_user_input.eui
+    import easy_user_input.easy_user_input #type: ignore
+    #pylance incorrectly reports missing import here, so it is silenced
+    assert easy_user_input.eui == easy_user_input.easy_user_input

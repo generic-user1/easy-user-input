fee0e3ddaa5e96a69396e3cebf85c0ccb29e52eb
generic-user1 <89677116+generic-user1@users.noreply.github.com>
2022-01-03 17:58:00 -0500

    Renamed alias testing functions

--- tests/test_eui.py
+++ tests/test_eui.py
@@ -2,7 +2,7 @@
 
 #test that `from easy_user_input import easy_user_input` is 
 #the same as `from easy_user_input import eui`
-def test_alias_from():
+def test_import_from_alias():
     from easy_user_input import eui
     from easy_user_input import easy_user_input
     assert eui == easy_user_input
@@ -10,7 +10,7 @@ def test_alias_from():
 
 #test that `import easy_user_input.easy_user_input` is 
 #the same as `import easy_user_input.eui`
-def test_alias_direct():
+def test_import_alias_direct():
     import easy_user_input.eui
     import easy_user_input.easy_user_input #type: ignore
     #pylance incorrectly reports missing import here, so it is silenced
